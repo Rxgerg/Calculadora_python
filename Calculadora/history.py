e28557f7fc16b85f1f1e21bb802d8196d3fb0b17
Rogelio Gomez <rxgerg@hotmail.com>
2025-05-12 19:56:01 -0600

    FEAT Se agregaron más funcionabilidades a la calculadora.

--- Calculadora/history.py
+++ Calculadora/history.py
@@ -1,3 +1,7 @@
+import os.path
+
+
+FILE_PATH = "history.txt"
 history_list = []
 
 #Guardar la operación en la memoria
@@ -14,6 +18,25 @@ def Show_History():
             print(op)
         
 #Borrar historial de memoria
-def Delete_History():
+def Clear_History():
     history_list.clear()
     print("Historial borrado.")
+    
+#Carga el historial si existe.
+def Load_History():
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            # Limpiar saltos de línea y cargar en lista
+            global history_list
+            history_list = [line.strip() for line in lines]
+
+#Guarda el historial
+def Save_History():
+    try:
+        with open(FILE_PATH, "w", encoding="utf-8") as file:
+            for operacion in history_list:
+                file.write(operacion + "\n")
+            print(f"Se ha guardado correctamente ", {FILE_PATH})
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
--- Calculadora/operations.py
+++ Calculadora/operations.py
@@ -1,17 +1,39 @@
+import math
+
 #Suma
 def Sum(a, b):
     return a + b
 
 #Resta
-def Resta(a, b):
+def Substract(a, b):
     return a - b
 
 #Multiplicacion
-def Multiplicacion(a, b) :
+def Multiply(a, b) :
     return a * b
 
 #Division
-def Division(a, b):
+def Divide(a, b):
     if b == 0:
         raise ValueError("Error: División entre cero no permitida.")
     return a / b
+
+#Potencia
+def Power(a,b):
+    return a ** b
+
+#Raíz cuadrada
+def Square_root(a):
+    if a < 0:
+        raise ValueError("No se le puede sacar raíz cuadrada a números negativos.")
+    return math.sqrt(a)
+
+#Módulo
+def Modulo(a,b):
+    return a % b
+
+#Factorial
+def Factorial(n):
+    if n < 0 or not float(n).is_integer():
+        raise ValueError("El factorial solo está definido para enteros no negativos.")
+    return math.factorial(int(n))
